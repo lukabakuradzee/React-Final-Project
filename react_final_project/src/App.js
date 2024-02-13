d00ad7c502aa42627e434be04e33fc795243bc66
lukabakuradzee <lukabakuradzee@gmail.com>
2024-02-13 18:31:18 +0400

    header

--- react_final_project/src/App.js
+++ react_final_project/src/App.js
@@ -7,12 +7,14 @@ import FavoriteMoviesModal from './components/MovieDetails/FavoriteMoviesModal';
 function App() {
   return (
     <div className="App">
+      <header>
       <Link to={HOME_PAGE}>
         <h1>
           <i class="fab fa-google-play"></i>JustWatch
         </h1>
       </Link>
       <FavoriteMoviesModal/>
+      </header>
       <AppRoutes />
     </div>
   );
